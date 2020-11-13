0217d978452e0123b6747290a492e6b587acec36
nsns0101 <nsns0101@naver.com>
2020-11-13 16:47:47 +0900

    [C]015.py

---
+++ python/[C]015.py
@@ -0,0 +1,24 @@
+# N 각 영수증의 수
+# 
+N = int(input())
+
+input_value = []
+for i in range(0, N):
+    input_value.append(input().split(" "))
+    
+
+point = 0
+for i in range(0, len(input_value)):
+    # if(list(input_value[i][0])[-1] == 3 or list(input_value[i][0])[-1] == 5):
+        # point += int( int(input_value[i][1]) / (100 * list(input_value[i][0])[-1]))
+    if "3" in list(input_value[i][0]):
+        # print("3")
+        point += int( int(input_value[i][1]) * 3 / 100)
+    elif "5" in list(input_value[i][0]):
+        # print("5")
+        point += int( int(input_value[i][1]) * 5 / 100)
+    else:
+        # print("else", int( int(input_value[i][1]) / 100))
+        point += int( int(input_value[i][1]) / 100)
+
+print(point)
