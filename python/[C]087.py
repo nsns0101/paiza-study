002d7b6365d38196c6cc9476a8213ec1cbd202ef
nsns0101 <nsns0101@naver.com>
2020-12-18 19:32:48 +0900

    common Update

--- python/[C]087.py
+++ python/[C]087.py
@@ -1,3 +1,9 @@
+#제일 높은자리 수와 낮은 자리수가 대칭되게 만드는 것
+#대칭되지않으면 수를 반전시켜 더하기
+# ex ) 261을 입력
+# 261 + 162 = 423 ===> 대칭x  ==> 계속 실행
+# 423 + 324 = 747 ===> 대칭o
+# 정답은 747
 input_value = int(input())
 
 #대칭 값이 아니면 값을 반대로하여 두 수를 더해 반환
@@ -11,8 +17,6 @@ while(True):
     # print(input_value)
     if(list(str(input_value))[0] == list(str(input_value))[-1]):
         break
-
-    
 
 print(input_value)
 
